5996893d55ab83256fc19a935b12cb264ad02c01
keepUpgogogo <1244342317@qq.com>
2022-04-25 22:44:46 +0800

    项目搭建

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true,
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'app.html',
+      inject: 'body',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    }]
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+
+  },
+}
